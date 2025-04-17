2f13c4bb3d5fb715e63abb1e0025b4d717668109
Marvin Ahlgrimm <marv.ahlgrimm@gmail.com>
2025-04-17 20:32:13 +0200

    Add DSL

--- spec/import_map_spec.cr
+++ spec/import_map_spec.cr
@@ -1,4 +1,35 @@
 require "./spec_helper"
 
+
+ImportMap.draw do
+  pin "stimulus", "/js/stimulus.js"
+
+  namespace "admin" do
+    pin "sort_controller", "/js/admin/sort_controller.js", preload: false
+  end
+end
+
 describe ImportMap do
+  it "populates manager via DSL" do
+    ImportMap::Manager.instance.json("admin").should eq(
+      {"imports" => {"stimulus" => "/js/stimulus.js", "sort_controller" => "/js/admin/sort_controller.js"}}.to_json
+    )
+  end
+
+  it "renders correct HTML tag block" do
+    html = ImportMap.tag("admin", "entrypoint")
+    html.includes?(%(<link rel="modulepreload" href="/js/stimulus.js">)).should be_true
+    html.includes?(%(<link rel="modulepreload" href="/js/admin/sort_controller.js">)).should be_false
+    html.includes?(%(<script type="importmap" data-namespace="admin">)).should be_true
+    html.includes?(%(<script type="module">import entrypoint</script>)).should be_true
+    html.includes?(%(<script type="importmap">)).should be_false
+  end
+
+  it "resolves paths correctly when setting a custom resolver" do
+    ImportMap.resolver = ->(path : String) { "/assets#{path}?v=abc" }
+    html = ImportMap.tag("admin", "entrypoint")
+    html.includes?(%(<link rel="modulepreload" href="/assets/js/stimulus.js?v=abc">)).should be_true
+
+    ImportMap.resolver = ->(path : String) { path }
+  end
 end
--- src/import_map.cr
+++ src/import_map.cr
@@ -3,4 +3,30 @@ require "./**"
 
 module ImportMap
   VERSION = "0.1.0"
+
+  def self.draw(&)
+    with ImportMap::Manager.instance yield
+  end
+
+  def self.resolver=(resolver : Proc(String, String))
+    ImportMap::Manager.instance.resolver = resolver
+  end
+
+  def self.tag(ns : String? = nil, entrypoint : String? = nil)
+    m = Manager.instance
+
+    json = m.json(ns)
+    preloads = m.preloads(ns)
+
+    String.build do |io|
+      preloads.each do |url|
+        io << %(<link rel="modulepreload" href="#{url}">\n)
+      end
+      data = ns ? %( data-namespace="#{ns}") : ""
+      io << %(<script type="importmap"#{data}>#{json}</script>\n)
+      if entrypoint
+        io << %(<script type="module">import #{entrypoint}</script>\n)
+      end
+    end
+  end
 end
